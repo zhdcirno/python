5a4b63e0de42b1f4c27eb406507bfa262bcb0a28
zhdcirno <1206903578@qq.com>
2018-09-14 07:25:11 +0800

    by zhdcirno

---
+++ 高级-包管理/p01.py
@@ -0,0 +1,16 @@
+# 包含一个学生类
+# 一个sayHello函数
+# 一个打印语句
+
+class Student():
+    def __init__(self,name = "NoName", age = 18):
+        self.name = name
+        self.age = age
+
+    def say(self):
+        print("My name is {0}".format(self.name))
+
+def sayHello():
+    print("你好，今天也是充满希望的一天")
+
+print("这里是模块01")
---
+++ 高级-包管理/p02.py
@@ -0,0 +1,7 @@
+import p01
+
+stu = p01.Student("wangdan", 20)
+
+stu.say()
+
+p01.sayHello()
